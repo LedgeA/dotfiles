89c295fe4d247c3d7f832edc1f4facb01bc1bf36
LedgeA <lancegaerlan04@gmail.com>
2025-06-30 23:40:34 +0800

    Update change-wp.sh

--- .scripts/change-wp.sh
+++ .scripts/change-wp.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-WALLPAPER_DIR="$HOME/Wallpapers"
+WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
 
 if [ "$1" = "-p" ]; then
-  WALLPAPER=$(find "$WALLPAPER_DIR" -type f | fzf --preview 'kitty +kitten icat --clear --transfer-mode=file --stdin=no --place=108x44@0x0 {}' --preview-window=right:60%)
-
+  wallpaper picker
+  WALLPAPER=$(find "$WALLPAPER_DIR" -type f | fzf \
+    --preview '
+      kitty +kitten icat \
+        --clear \
+        --transfer-mode=file \
+        --stdin=no \
+        --place=$((COLUMNS))x$((LINES))@0x0 \
+        {}
+    ' \
+    --preview-window=right:60%:wrap \
+    --bind 'ctrl-r:toggle-preview+toggle-preview' \
+    --header 'Refresh: Ctrl+R'
+  )
 else
   # find a random wallpaper that isn't the current wallpaper
   CURRENT_WALL=$(swww query | grep 'currently displaying:' | sed -E 's/.*image: //') 
@@ -15,26 +27,10 @@ fi
 [ -z "$WALLPAPER" ] && exit
 
 # Generate pywal colors
-wal -i "$WALLPAPER"
+wal -i "$WALLPAPER" --cols16 lighten
 
 # reload wallpaper
 swww img --transition-type wipe --transition-angle 30 --transition-step 200 "$WALLPAPER" 
 
 # reload mako
 makoctl reload
-
-# retrieve pywal colors
-source ~/.cache/wal/colors.sh
-
-# hyprlock & hyprland
-OUT="$HOME/.config/hypr/colors.conf"
-TMP="$(mktemp)"
-
-# Create the .conf file
-cat > "$TMP" <<EOF
-\$WALLPAPER=$WALLPAPER
-\$background=rgba(${background:1}ff)
-\$foreground=rgba(${foreground:1}BF)
-EOF
-
-mv "$TMP" "$OUT"
