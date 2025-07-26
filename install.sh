8a56680a41346a3b73c3029f067cdba0e41e6629
LedgeA <lancegaerlan04@gmail.com>
2025-07-26 21:17:00 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,48 @@
+sudo pacman -S swww hyprlock waybar mako nwg-bar hyprshot fzf
+sudo pacman -S ttf-font-awesome ttf-jetbrains-mono-nerd noto-fonts-cjk
+
+echo "setting up bluetooth"
+sudo pacman -S bluez bluez-utils blueman
+sudo systemctl enable --now bluetooth.service
+
+echo "setting up audio"
+sudo pacman -S pipewire wireplumber pavucontrol pipewire-pulse pipewire-alsa 
+sudo systemctl --user enable --now pipewire
+sudo systemctl --user enable --now wireplumber
+
+echo "installing and setting up git and paru"
+mkdir -p ~/builds 
+cd ~/builds 
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+echo "installing zen-browser and pywal16"
+paru -S zen-browser-bin python-pywal16
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+DIRS=(
+  ".config/hypr"
+  ".config/fuzzel"
+  ".config/mako"
+  ".config/waybar"
+  ".config/nwg-bar"
+  ".config/kitty"
+  ".config/wal"
+  ".cache/wal" # 
+  ".scripts"
+)
+
+echo "copying dotfiles"
+for dir in "${DIRS[@]}"; do
+  cp -r "$SCRIPT_DIR/$dir" "$HOME/$dir"
+done
+
+echo "symlinking fuzzel and mako configs"
+ln -sf ~/.cache/wal/colors-fuzzel.ini ~/.config/fuzzel/fuzzel.ini
+ln -sf ~/.cache/wal/colors-mako ~/.config/mako/config
+
+echo "making .scripts files executable"
+chmod +x ~/.scripts/change-wp.sh
+chmod +x ~/.scripts/cliphist-fuzzel-img
