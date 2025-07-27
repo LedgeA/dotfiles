a4a7192427e04f1c9622bdd0e55ddc5638d18250
LedgeA <lancegaerlan04@gmail.com>
2025-07-27 17:24:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,28 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-sudo pacman -S --needed swww hyprlock waybar mako nwg-bar hyprshot fzf
+echo "## Installing programs ##"
+sudo pacman -S --needed hyprland git swww hyprlock waybar mako nwg-bar hyprshot fzf
+
+echo "## Installing fonts ##"
 sudo pacman -S --needed ttf-font-awesome ttf-jetbrains-mono-nerd noto-fonts-cjk
 
-echo "setting up bluetooth"
+echo "## Setting up bluetooth ##"
 sudo pacman -S --needed bluez bluez-utils blueman
 sudo systemctl enable --now bluetooth.service
 
-echo "setting up audio"
+echo "## Setting up audio ##"
 sudo pacman -S --needed pipewire wireplumber pavucontrol pipewire-pulse pipewire-alsa 
 sudo systemctl --user enable --now pipewire
 sudo systemctl --user enable --now wireplumber
 
 if ! pacman -Q paru &>/dev/null; then
-  echo "paru not found, installing..."
+  echo "## Installing paru ##"
   mkdir -p ~/builds
   git clone https://aur.archlinux.org/paru.git ~/builds/paru
   cd ~/builds/paru && makepkg -si --noconfirm
 fi
 
-echo "installing zen-browser and pywal16"
+echo "## Installing zen-browser and pywal16 ##"
 paru -S --needed zen-browser-bin python-pywal16
 
 DIRS=(
@@ -35,15 +38,15 @@ DIRS=(
   "Pictures/Wallpapers"
 )
 
-echo "copying dotfiles"
+echo "## Copying dotfiles ##"
 for dir in "${DIRS[@]}"; do
   cp -r "$SCRIPT_DIR/$dir" "$HOME/"
 done
 
-echo "symlinking fuzzel and mako configs"
+echo "## Symlinking fuzzel and mako configs ##"
 ln -sf ~/.cache/wal/colors-fuzzel.ini ~/.config/fuzzel/fuzzel.ini
 ln -sf ~/.cache/wal/colors-mako ~/.config/mako/config
 
-echo "making .scripts files executable"
+echo "## Making .scripts files executable ##"
 chmod +x ~/.scripts/change-wp.sh
 chmod +x ~/.scripts/cliphist-fuzzel-img
